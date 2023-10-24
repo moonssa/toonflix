286836b628e47e0e01a6d490418ae5cbb9f86fc4
moonk <mkjoehsb@gmail.com>
2023-10-24 11:55:41 +0900

    button section

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class App extends StatelessWidget {
               horizontal: 40,
             ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
                   height: 80,
@@ -39,6 +40,48 @@ class App extends StatelessWidget {
                               fontSize: 18,
                             )),
                       ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 120,
+                ),
+                Text('Total Balance',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white.withOpacity(0.7),
+                    )),
+                SizedBox(
+                  height: 5,
+                ),
+                Text('\$5 194 482',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 44,
+                      color: Colors.white,
+                    )),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 50,
+                        ),
+                        child: Text(
+                          'Transfer',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
                     )
                   ],
                 )
