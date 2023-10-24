c7970196da18578c533ec67204cf86caa1b399c2
moonk <mkjoehsb@gmail.com>
2023-10-24 09:17:49 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Text("Hello flutter"),
+        ),
+        body: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
