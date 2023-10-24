2f9646b62695623b94e383a0802b31e1f391464e
moonk <mkjoehsb@gmail.com>
2023-10-24 11:11:33 +0900

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,45 @@ void main() {
 }
 
 class App extends StatelessWidget {
+  // String name;
+  // App(this.name);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          title: Text("Hello flutter"),
-        ),
-        body: Center(
-          child: Text("Hello World"),
-        ),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 40,
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text('Hey, Selena',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w800,
+                            )),
+                        Text('Welcome back',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontSize: 18,
+                            )),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            )),
       ),
     );
   }
