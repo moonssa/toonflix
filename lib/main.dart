0a5202ad2d2ad190dd7255d0b3b6acb5dc443209
rebootedCoder2 <oborimoon@gmail.com>
2024-05-08 22:03:57 +0900

    pomodoro-ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<int> numbers = [];
-
-  void onPressed() {
-    setState(() {
-      numbers.add(numbers.length);
-    });
-  }
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: const Color(0xFFF4EDDB),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Click Count",
-              style: TextStyle(
-                fontSize: 30,
-              ),
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            backgroundColor: const Color(0xFFE7626C),
+          ),
+
+          textTheme: const TextTheme(
+            headlineMedium: TextStyle(
+              color: Color(0xFF232B55),
             ),
-            for (var n in numbers) Text("$n"),
-            IconButton(
-              onPressed: onPressed,
-              icon: const Icon(Icons.add_box_outlined),
-              iconSize: 40,
-            )
-          ],
+          ),
+          // textTheme: const TextTheme(
+          //   displayLarge: TextStyle(
+          //     color: Color(0xFF232B55),
+          //   ),
+          // ),
+          cardColor: const Color(0xFFF4EDDB),
         ),
-      ),
-    ));
+        home: const HomeScreen());
   }
 }
