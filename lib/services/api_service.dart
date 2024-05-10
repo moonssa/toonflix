075440d315b33e309d6947e364539c9fe2828b6e
rebootedCoder2 <oborimoon@gmail.com>
2024-05-10 17:50:17 +0900

    API getToonById, getLatestEpisodesById 추가

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,9 @@
 import "dart:convert";
 
 import "package:http/http.dart" as http;
+import "package:toonflix/models/webtoon_episode_model.dart";
 import "package:toonflix/models/webtoon_model.dart";
+import "package:toonflix/models/wentoon_detail_model.dart";
 
 class ApiService {
   static const String baseUrl =
@@ -21,6 +23,31 @@ class ApiService {
       }
 
       return webtoonInstance;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      List<WebtoonEpisodeModel> episodesInstances = [];
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
